2c84d3a98a04ac2a20bf6f5fb5ebba4add36e01e
Charles De Lafontaine <DaddyChucky@users.noreply.github.com>
2020-09-21 13:19:20 -0400

    Completed all exercises

--- exercice.py
+++ exercice.py
@@ -6,26 +6,51 @@ from typing import List
 
 
 def convert_to_absolute() -> float:
-    return 0.0
+    x = -5.5
+    return abs(x)
 
 
 def use_prefixes() -> List[str]:
     prefixes, suffixes = 'JKLMNOP', 'ack'
-
-    return [""]
+    list = []
+    for letters in prefixes:
+        list.append(letters + suffixes)
+    return list
 
 
 def prime_integer_summation() -> int:
-    return 0
+    count = 0
+    somme = 0
+    liste_premiers = []
+    for i in range(2,101):
+        for z in range(2,101):
+            if z <= i:
+                if i % z == 0:
+                    count += 1
+        if count == 1:
+            liste_premiers.append(i)
+        count = 0
 
+    for i in liste_premiers:
+        somme += i
+    return somme, liste_premiers
 
 def factorial(number: int) -> int:
-    return 0
+    factoriel = 1
+    for i in range(1, number+1):
+        factoriel *= i
+    return factoriel
 
 
 def use_continue() -> None:
-    pass
-
+    list = []
+    for i in range(1,11):
+        if i == 5:
+            continue
+        else:
+            pass
+        list.append(i)
+    return list
 
 def main() -> None:
     print(f"La valeur absolue du nombre est {convert_to_absolute()}")
@@ -37,8 +62,7 @@ def main() -> None:
     number = 10
     print(f"La factiorelle du nombre {number} est: {factorial(number)}")
     
-    print(f"L'affichage de la boucle est:")
-    use_continue()
+    print(f"L'affichage de la boucle est: {use_continue()}")
 
 
 if __name__ == '__main__':
